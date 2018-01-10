2a074e3f99e6ff7274a5e1b38d3dc170275c7adb
juliocarneiro <juliocarneiro@gmail.com>
2018-01-10 13:03:22 -0200

    initial

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.scss'
+
+const App = () => <div>Hello Parcel x React</div>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
