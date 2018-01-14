f8b6fba54ac954e86c3df12cebf2084e3c2aef9f
Julio Carneiro <juliocarneiro@gmail.com>
2018-01-13 22:47:41 -0200

    correcao

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ import './styles.scss'
 const App = () => <div>Hello Parcel x React</div>;
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+if (module.hot) {
+    module.hot.accept();
+}
